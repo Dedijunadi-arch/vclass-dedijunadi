bd53e67a14475d7b5fc11f4f6535b6e903ce1ae9
Dedijunadi-arch <62701421+Dedijunadi-arch@users.noreply.github.com>
2020-04-02 19:48:43 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,10 @@
+int main(int argc, char const *argv[])
+{
+	Mahasiswa mahasiswa1 = Mahasiswa("Dedi Junadi", "11118711", "2KA30", "Sistem Informasi"); 
+	cout <<"\n";
+	Mahasiswa mahasiswa2 = Mahasiswa("Pandy Muhamad", "12528711", "2KA29", "Teknik Informatika");
+	cout <<"\n";
+	Mahasiswa mahasiswa3 = Mahasiswa("Thoriq Mahfud", "12522153", "2KA28", "Teknik Komputer"); 
+
+return 0;
+}
